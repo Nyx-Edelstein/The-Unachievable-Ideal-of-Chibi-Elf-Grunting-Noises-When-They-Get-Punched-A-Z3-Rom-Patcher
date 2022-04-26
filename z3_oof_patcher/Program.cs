2cdccb4179b4ce35586c7c64ac595f9d77749f3f
Nyx-Edelstein <91574183+Nyx-Edelstein@users.noreply.github.com>
2022-04-26 02:56:57 -0700

    attempted to patch to archipelago, didn't work
    giving up for now

--- z3_oof_patcher/Program.cs
+++ z3_oof_patcher/Program.cs
@@ -7,6 +7,7 @@
             var romFilename = "";
             var sampleFilename = "";
             var outputFilename = "";
+            var archipelago = false;
 
             for (var i = 0; i < args.Length - 1; i++)
             {
@@ -25,6 +26,12 @@
                 }
             }
 
+            //foreach (var arg in args)
+            //{
+            //    if (arg == "--ap")
+            //        archipelago = true;
+            //}
+
             try
             {
                 //Ease of use feature: patch the first (presumably only) .sfc file in the directory if there are no arguments
@@ -41,7 +48,7 @@
                 if (outputFilename == string.Empty)
                     outputFilename = "patched_" + romFilename;
 
-                ApplyPatch(romFile, sampleFile, outputFilename);
+                ApplyPatch(romFile, sampleFile, outputFilename, archipelago);
             }
             catch (Exception e)
             {
@@ -135,9 +142,9 @@
             return bytes;
         }
 
-        private static void ApplyPatch(byte[] romFile, byte[] sampleFile, string outputFilename)
+        private static void ApplyPatch(byte[] romFile, byte[] sampleFile, string outputFilename, bool archipelago)
         {
-            var patches = GetPatches(sampleFile);
+            var patches = GetPatches(sampleFile, archipelago);
 
             foreach (var patch in patches)
             {
@@ -152,9 +159,12 @@
             File.WriteAllBytes(outputFilename, romFile);
             Console.WriteLine("Patch successful.");
         }
-        private static List<Patch> GetPatches(byte[] customSample)
+        private static List<Patch> GetPatches(byte[] customSample, bool archipelago)
         {
             var patches = new List<Patch>();
+
+            var h = customSample.Length.ToString("X4");
+            var XXXX = string.Join("", h[2], h[3], h[0], h[1]);
 
             //Credit to witch princess kan on alttpr discord for an improved method over what I was doing
             //The below patches are based on their code
@@ -162,35 +172,50 @@
             //Jump execution from the SPC load routine to new code
             patches.Add(new Patch(0x8CF, "5C008025"));
 
-            //Change the pointer for instrument $9 in SPC memory to point to the new data we'll be inserting:
-            patches.Add(new Patch(0xC806C, "88310000"));
+            ////Presumably ALTTPR
+            //if (!archipelago)
+            //{
+                //Change the pointer for instrument $9 in SPC memory to point to the new data we'll be inserting:
+                patches.Add(new Patch(0xC806C, "88310000"));
 
-            //Insert a sigil so we can branch on it later; this overwrites unused data
-            patches.Add(new Patch(0xCFB18, "BEBE"));
+                //Insert a sigil so we can branch on it later; this overwrites unused data
+                patches.Add(new Patch(0xCFB18, "BEBE"));
 
-            //Change the "oof" sound effect to use instrument $9:
-            patches.Add(new Patch(0xD1BF5, "09"));
+                //Change the "oof" sound effect to use instrument $9:
+                patches.Add(new Patch(0xD1BF5, "09"));
 
-            //Correct pitch shift value:
-            patches.Add(new Patch(0xD1BF8, "B6"));
+                //Correct pitch shift value:
+                patches.Add(new Patch(0xD1BF8, "B6"));
 
-            //Modify parameters of instrument $9
-            //(I don't actually understand this part, they're just magic values to me)
-            patches.Add(new Patch(0xD1C55, "7F7F00101A00007F01"));
+                //Modify parameters of instrument $9
+                //(I don't actually understand this part, they're just magic values to me)
+                patches.Add(new Patch(0xD1C55, "7F7F00101A00007F01"));
 
-            //Hook from SPC load routine:
-            // * Check for the read of the sigil
-            // * Once we find it, change the SPC load routine's data pointer to read from the location containing the new sample
-            // * Note: XXXX in the string below is a placeholder for the number of bytes in the .brr sample (little endian)
-            // * Another sigil "$EBEB" is inserted at the end of the data
-            // * When the second sigil is read, we know we're done inserting our data so we can change the data pointer back
-            // * Effect: The new data gets loaded into SPC memory without having to relocate the SPC load routine
-            var h = customSample.Length.ToString("X4");
-            Console.WriteLine(h);
-            var XXXX = string.Join("", h[2], h[3], h[0], h[1]);
-            Console.WriteLine(XXXX);
-            var byteStr = $"B700C8C8C9BEBEF009C9EBEBF01B5CD38800A2{XXXX}A980258501A93A808500A00000A988315CD88800A9801964008501A2AE00A01A7B5CD48800";
-            patches.Add(new Patch(0x128000, byteStr));
+                //Hook from SPC load routine:
+                // * Check for the read of the sigil
+                // * Once we find it, change the SPC load routine's data pointer to read from the location containing the new sample
+                // * Note: XXXX in the string below is a placeholder for the number of bytes in the .brr sample (little endian)
+                // * Another sigil "$EBEB" is inserted at the end of the data
+                // * When the second sigil is read, we know we're done inserting our data so we can change the data pointer back
+                // * Effect: The new data gets loaded into SPC memory without having to relocate the SPC load routine
+                var byteStr = $"B700C8C8C9BEBEF009C9EBEBF01B5CD38800A2{XXXX}A980258501A93A808500A00000A988315CD88800A9801964008501A2AE00A01A7B5CD48800";
+                patches.Add(new Patch(0x128000, byteStr));
+            //}
+            //else
+            //{
+            //    //Offsets need to change to accommodate archipelago roms; otherwise same as the above
+
+            //    //We actually can't use $3188 in SPC memory because it's in use by something else
+            //    //Have to use the smaller buffer from $BAA0 to $BFF0 instead
+            //    patches.Add(new Patch(0x1A006C, "A0BA0000"));
+            //    patches.Add(new Patch(0x1A7B18, "BEBE"));
+            //    patches.Add(new Patch(0x1A9C4E, "09"));
+            //    patches.Add(new Patch(0x1A9C51, "B6"));
+            //    patches.Add(new Patch(0x1A9CAE, "7F7F00101A00007F01"));
+                
+            //    var byteStr = $"B700C8C8C9BEBEF009C9EBEBF01B5CD38800A2{XXXX}A980258501A93A808500A00000A9A0BA5CD88800A9801964008501A2AE00A01A7B5CD48800";
+            //    patches.Add(new Patch(0x128000, byteStr));
+            //}
 
             //The new sample data
             //(We need to insert the second sigil at the end)
